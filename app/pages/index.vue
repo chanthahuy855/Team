cb53807fcc50e11896e9711d902d48819ecacc81
Huy Chantha <chantha.huy@canadiabank.com.kh>
2025-12-04 10:23:52 +0700

    first commit

---
+++ app/pages/index.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+    <!-- <UPageHero
+      title="Nuxt Starter Template"
+      description="A production-ready starter template powered by Nuxt UI. Build beautiful, accessible, and performant applications in minutes, not hours."
+    
+    /> -->
+
+    <UPageSection>
+
+      <UPageCTA
+        title="Ready to build your next Nuxt app?"
+        description="Join thousands of developers building with Nuxt and Nuxt UI. Get this template and start shipping today."
+        variant="subtle"
+        :links="[{
+          label: 'Start building',
+          to: 'https://ui.nuxt.com/docs/getting-started/installation/nuxt',
+          target: '_blank',
+          trailingIcon: 'i-lucide-arrow-right',
+          color: 'neutral'
+        }, {
+          label: 'View on GitHub',
+          to: 'https://github.com/nuxt-ui-templates/starter',
+          target: '_blank',
+          icon: 'i-simple-icons-github',
+          color: 'neutral',
+          variant: 'outline'
+        }]"
+      />
+    </UPageSection>
+    
+  </div>
+</template>
